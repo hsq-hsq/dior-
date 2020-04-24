7990ddc281fdb4519e3cba7b81af0ae0b8076065
hsq-hsq <64236774+hsq-hsq@users.noreply.github.com>
2020-04-24 09:37:35 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+//引入路由器
+//const userRouter=require('./routes/user.js');
+//const productRouter=require('./routes/product.js');
+const cartRouter=require('./routes/cart.js');
+let app=express();
+app.listen(8888);
+//托管静态资源
+app.use( express.static('public') );
+//使用body-parser中间件
+app.use( bodyParser.urlencoded({
+	extended:false
+}) );
+//把路由器挂载到服务器，给URL添加前缀
+//app.use('/user',userRouter);
+//app.use('/product',productRouter);
+app.use('/cart',cartRouter);
