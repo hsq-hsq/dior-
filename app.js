dded68a30ca1c5873d378c2e18ca8680a9f9a61f
hsq-hsq <64236774+hsq-hsq@users.noreply.github.com>
2020-04-24 09:57:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 //引入路由器
-//const userRouter=require('./routes/user.js');
-//const productRouter=require('./routes/product.js');
+const userRouter=require('./routes/user.js');
+const productRouter=require('./routes/product.js');
 const cartRouter=require('./routes/cart.js');
 let app=express();
 app.listen(8888);
@@ -13,6 +13,6 @@ app.use( bodyParser.urlencoded({
 	extended:false
 }) );
 //把路由器挂载到服务器，给URL添加前缀
-//app.use('/user',userRouter);
-//app.use('/product',productRouter);
+app.use('/user',userRouter);
+app.use('/product',productRouter);
 app.use('/cart',cartRouter);
